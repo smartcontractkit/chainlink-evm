68990d0c81c42aba1fff35ed61617fef05c31160
Ilan <ilan.olkies@smartcontract.com>
2025-04-28 16:15:14 -0300

    Generate necessary mocks

--- gethwrappers/zksync/go_generate.go
+++ gethwrappers/zksync/go_generate.go
@@ -5,5 +5,8 @@ package gethwrappers
 //go:generate go run ../generation/zksync/wrap.go shared LinkToken link_token
 //go:generate go run ../generation/zksync/wrap.go shared BurnMintERC677 burn_mint_erc677
 //go:generate go run ../generation/zksync/wrap.go shared Multicall3 multicall3
-//go:generate go run ../generation/zksync/wrap.go shared WETH9 weth9
 //go:generate go run ../generation/zksync/wrap.go keystone CapabilitiesRegistry capabilities_registry
+
+//go:generate go run ../generation/zksync/wrap.go shared WETH9 weth9
+//go:generate go run ../generation/zksync/wrap.go shared MockV3Aggregator mock_v3_aggregator_contract
+//go:generate go run ../generation/zksync/wrap.go automation MockETHUSDAggregator mock_ethusd_aggregator_wrapper
