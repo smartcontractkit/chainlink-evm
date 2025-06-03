a3250d61061b6281605e7450df845ae708570e2a
Domino Valdano <2644901+reductionista@users.noreply.github.com>
2025-06-02 20:10:14 -0700

    Update test

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -353,7 +353,7 @@ func TestChainScopedConfig_HeadTracker(t *testing.T) {
 	assert.Equal(t, uint32(100), ht.HistoryDepth())
 	assert.Equal(t, uint32(3), ht.MaxBufferSize())
 	assert.Equal(t, time.Second, ht.SamplingInterval())
-	assert.True(t, ht.FinalityTagBypass())
+	assert.False(t, ht.FinalityTagBypass())
 	assert.Equal(t, uint32(10000), ht.MaxAllowedFinalityDepth())
 	assert.True(t, ht.PersistenceEnabled())
 }
