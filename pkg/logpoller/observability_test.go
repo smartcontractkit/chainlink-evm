e6399856ecbf411e402d8899644a124f43de9b3f
Vladimir Shchukin <vladimir.shchukin@smartcontract.com>
2025-06-18 11:03:33 -0400

    use parent context for observability

--- pkg/logpoller/observability_test.go
+++ pkg/logpoller/observability_test.go
@@ -71,7 +71,7 @@ func TestMetricsAreProperlyPopulatedWithLabels(t *testing.T) {
 	expectedSize := 2
 
 	for i := 0; i < expectedCount; i++ {
-		_, err := withObservedQueryAndResults(orm, "query", func() ([]string, error) { return []string{"value1", "value2"}, nil })
+		_, err := withObservedQueryAndResults(t.Context(), orm, "query", func() ([]string, error) { return []string{"value1", "value2"}, nil })
 		require.NoError(t, err)
 	}
 
@@ -88,7 +88,7 @@ func TestMetricsAreProperlyPopulatedWithLabels(t *testing.T) {
 func TestNotPublishingDatasetSizeInCaseOfError(t *testing.T) {
 	orm := createObservedORM(t, 420)
 
-	_, err := withObservedQueryAndResults(orm, "errorQuery", func() ([]string, error) { return nil, errors.New("error") })
+	_, err := withObservedQueryAndResults(t.Context(), orm, "errorQuery", func() ([]string, error) { return nil, errors.New("error") })
 	require.Error(t, err)
 
 	require.Equal(t, 1, counterFromHistogramByLabels(t, orm.queryDuration, network, "420", "errorQuery", "read"))
@@ -97,8 +97,8 @@ func TestNotPublishingDatasetSizeInCaseOfError(t *testing.T) {
 
 func TestMetricsAreProperlyPopulatedForWrites(t *testing.T) {
 	orm := createObservedORM(t, 420)
-	require.NoError(t, withObservedExec(orm, "execQuery", metrics.Create, func() error { return nil }))
-	require.Error(t, withObservedExec(orm, "execQuery", metrics.Create, func() error { return errors.New("error") }))
+	require.NoError(t, withObservedExec(t.Context(), orm, "execQuery", metrics.Create, func() error { return nil }))
+	require.Error(t, withObservedExec(t.Context(), orm, "execQuery", metrics.Create, func() error { return errors.New("error") }))
 
 	require.Equal(t, 2, counterFromHistogramByLabels(t, orm.queryDuration, network, "420", "execQuery", "create"))
 }
