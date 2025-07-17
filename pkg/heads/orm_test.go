08ab3111d25c5ebc14b8348ff14bd551510d3c74
yashnevatia <yashvardhan.nevatia@smartcontract.com>
2025-07-17 11:59:54 +0100

    docs

--- pkg/heads/orm_test.go
+++ pkg/heads/orm_test.go
@@ -1,7 +1,6 @@
 package heads_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -119,52 +118,6 @@ func TestORM_TrimOldHeads_Batch(t *testing.T) {
 	for i := 0; i < 3; i++ {
 		require.LessOrEqual(t, int64(7), heads[i].Number)
 	}
-}
-
-func TrimBlocks(blocks []int, minBlockNumber int) []int {
-	for i, block := range blocks {
-		if block >= minBlockNumber {
-			return blocks[i:]
-		}
-	}
-	// All blocks are less than minBlockNumber
-	return []int{}
-}
-
-var lastTrimmedBlockNumber int64
-
-func trim(minBlockNumber int64, batchSize int64, db []int) []int {
-	if lastTrimmedBlockNumber == -1 {
-		lastTrimmedBlockNumber = minBlockNumber - 1
-	}
-	if minBlockNumber-lastTrimmedBlockNumber <= batchSize {
-		return db
-	}
-	lastTrimmedBlockNumber = minBlockNumber - 1
-	db = TrimBlocks(db, int(minBlockNumber))
-	return db
-}
-
-func TestTrim(t *testing.T) {
-	lastTrimmedBlockNumber = -1
-	db := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	batchSize := int64(2)
-	db = trim(1, batchSize, db)
-	fmt.Println(1, db)
-	db = trim(2, batchSize, db)
-	fmt.Println(2, db)
-	db = trim(3, batchSize, db)
-	fmt.Println(3, db)
-	db = trim(4, batchSize, db)
-	fmt.Println(4, db)
-	db = trim(5, batchSize, db)
-	fmt.Println(5, db)
-	db = trim(6, batchSize, db)
-	fmt.Println(6, db)
-	db = trim(7, batchSize, db)
-	fmt.Println(7, db)
-	db = trim(8, batchSize, db)
-	fmt.Println(8, db)
 }
 
 func TestORM_HeadByHash(t *testing.T) {
