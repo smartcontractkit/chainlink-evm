4c24b8aa467916213f80e383bc6b535acb49eb91
Juan Lautaro Fernandez <juan.lautarofernandez@smartcontract.com>
2025-08-11 11:58:46 -0300

    PLEX-1621: adding logs (#185)
    * PLEX-1621: adding logs
    
    * fix test
    
    * warn->info

--- pkg/logpoller/log_poller_internal_test.go
+++ pkg/logpoller/log_poller_internal_test.go
@@ -59,7 +59,7 @@ func TestLogPoller_RegisterFilter(t *testing.T) {
 	a1 := common.HexToAddress("0x2ab9a2dc53736b361b72d900cdf9f78f9406fbbb")
 	a2 := common.HexToAddress("0x2ab9a2dc53736b361b72d900cdf9f78f9406fbbc")
 
-	lggr, observedLogs := logger.TestObserved(t, zapcore.WarnLevel)
+	lggr, observedLogs := logger.TestObserved(t, zapcore.InfoLevel)
 	chainID := testutils.NewRandomEVMChainID()
 	db := testutils.NewSqlxDB(t)
 	ctx := testutils.Context(t)
@@ -85,6 +85,8 @@ func TestLogPoller_RegisterFilter(t *testing.T) {
 	assert.Equal(t, []common.Address{a1}, lp.Filter(nil, nil, nil).Addresses)
 	assert.Equal(t, [][]common.Hash{{EmitterABI.Events["Log1"].ID}}, lp.Filter(nil, nil, nil).Topics)
 	validateFiltersTable(t, lp, orm)
+	require.Equal(t, 1, observedLogs.Len())
+	require.Contains(t, observedLogs.All()[0].Entry.Message, "Inserted filter")
 
 	// Should de-dupe EventSigs
 	err = lp.RegisterFilter(ctx, Filter{Name: "Emitter Log 1 + 2", EventSigs: []common.Hash{EmitterABI.Events["Log1"].ID, EmitterABI.Events["Log2"].ID}, Addresses: []common.Address{a2}})
@@ -92,6 +94,8 @@ func TestLogPoller_RegisterFilter(t *testing.T) {
 	assert.Equal(t, []common.Address{a1, a2}, lp.Filter(nil, nil, nil).Addresses)
 	assert.Equal(t, [][]common.Hash{{EmitterABI.Events["Log1"].ID, EmitterABI.Events["Log2"].ID}}, lp.Filter(nil, nil, nil).Topics)
 	validateFiltersTable(t, lp, orm)
+	require.Equal(t, 2, observedLogs.Len())
+	require.Contains(t, observedLogs.All()[1].Entry.Message, "Inserted filter")
 
 	// Should de-dupe Addresses
 	err = lp.RegisterFilter(ctx, Filter{Name: "Emitter Log 1 + 2 dupe", EventSigs: []common.Hash{EmitterABI.Events["Log1"].ID, EmitterABI.Events["Log2"].ID}, Addresses: []common.Address{a2}})
@@ -99,6 +103,8 @@ func TestLogPoller_RegisterFilter(t *testing.T) {
 	assert.Equal(t, []common.Address{a1, a2}, lp.Filter(nil, nil, nil).Addresses)
 	assert.Equal(t, [][]common.Hash{{EmitterABI.Events["Log1"].ID, EmitterABI.Events["Log2"].ID}}, lp.Filter(nil, nil, nil).Topics)
 	validateFiltersTable(t, lp, orm)
+	require.Equal(t, 3, observedLogs.Len())
+	require.Contains(t, observedLogs.All()[2].Entry.Message, "Inserted filter")
 
 	// Address required.
 	err = lp.RegisterFilter(ctx, Filter{Name: "no address", EventSigs: []common.Hash{EmitterABI.Events["Log1"].ID}})
@@ -111,8 +117,8 @@ func TestLogPoller_RegisterFilter(t *testing.T) {
 	// Removing non-existence Filter should log error but return nil
 	err = lp.UnregisterFilter(ctx, "Filter doesn't exist")
 	require.NoError(t, err)
-	require.Equal(t, 1, observedLogs.Len())
-	require.Contains(t, observedLogs.TakeAll()[0].Entry.Message, "not found")
+	require.Equal(t, 4, observedLogs.Len())
+	require.Contains(t, observedLogs.All()[3].Entry.Message, "not found")
 
 	// Check that all filters are still there
 	_, ok := lp.filters["Emitter Log 1"]
