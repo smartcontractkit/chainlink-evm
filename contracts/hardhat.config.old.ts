cb3579ee6e8d57d038a89b7e4a56fce2aee12730
Rens Rooimans <github@rensrooimans.nl>
2025-08-26 12:29:53 +0200

    HH v3

--- contracts/hardhat.config.old.ts
+++ contracts/hardhat.config.old.ts
@@ -1,4 +1,11 @@
-import type { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-ethers'
+import '@nomicfoundation/hardhat-verify'
+import '@nomicfoundation/hardhat-chai-matchers'
+import '@typechain/hardhat'
+import 'hardhat-abi-exporter'
+import { subtask } from 'hardhat/config'
+import path from 'path'
+import { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } from 'hardhat/builtin-tasks/task-names'
 
 const COMPILER_SETTINGS = {
   optimizer: {
@@ -10,12 +17,57 @@ const COMPILER_SETTINGS = {
   },
 }
 
-const config: HardhatUserConfig = {
+const EXCLUDE_DIRS = [
+  'src/v0.8/vendor/forge-std',
+  'src/v0.8/workflow',
+  'src/v0.8/vrf',
+  'src/v0.8/llo-feeds',
+  'src/v0.8/keystone',
+  'src/v0.8/functions',
+  'src/v0.8/vendor/@eth-optimism',
+]
+
+// prune forge style tests from hardhat paths
+subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(
+  async (_, __, runSuper) => {
+    const paths = await runSuper()
+    return paths
+      .filter((p: string) => !p.endsWith('.t.sol'))
+      .filter(
+        (p: string) =>
+          !EXCLUDE_DIRS.some((dir) => p.includes(path.normalize(dir))),
+      )
+  },
+)
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+let config = {
+  abiExporter: {
+    path: './abi',
+    runOnCompile: true,
+  },
   paths: {
     artifacts: './artifacts',
     cache: './cache',
     sources: './src',
     tests: './test',
+  },
+  typechain: {
+    outDir: './typechain',
+    target: 'ethers-v5',
+  },
+  networks: {
+    env: {
+      url: process.env.NODE_HTTP_URL || '',
+    },
+    hardhat: {
+      allowUnlimitedContractSize: Boolean(
+        process.env.ALLOW_UNLIMITED_CONTRACT_SIZE,
+      ),
+      hardfork: 'merge',
+    },
   },
   solidity: {
     compilers: [
@@ -97,6 +149,22 @@ const config: HardhatUserConfig = {
       },
     },
   },
+  mocha: {
+    timeout: 150000,
+    forbidOnly: Boolean(process.env.CI),
+  },
+  warnings: !process.env.HIDE_WARNINGS,
+}
+
+if (process.env.NETWORK_NAME && process.env.EXPLORER_API_KEY) {
+  config = {
+    ...config,
+    etherscan: {
+      apiKey: {
+        [process.env.NETWORK_NAME]: process.env.EXPLORER_API_KEY,
+      },
+    },
+  }
 }
 
 export default config
