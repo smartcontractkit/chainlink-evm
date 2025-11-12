0596cb1a5e83487a0c6eb22bd713bd0d9babeba7
Rens Rooimans <github@rensrooimans.nl>
2025-11-12 14:01:17 +0100

    add fake imports
    gomidtidy

--- contracts/cre/gobindings/go_generate.go
+++ contracts/cre/gobindings/go_generate.go
@@ -2,6 +2,11 @@
 // golang packages, using abigen.
 package gobindings
 
+import (
+	_ "github.com/smartcontractkit/chainlink-evm/gethwrappers/helpers"
+	_ "github.com/smartcontractkit/chainlink-evm/gethwrappers/helpers/zksync"
+)
+
 // Make sure solidity compiler artifacts are up-to-date. Only output stdout on failure.
 //go:generate ../scripts/compile_all_keystone
 //go:generate ../scripts/compile_all_workflow
--- gethwrappers/helpers/go.mod
+++ gethwrappers/helpers/go.mod
@@ -1,6 +1,6 @@
 module github.com/smartcontractkit/chainlink-evm/gethwrappers/helpers
 
-go 1.25
+go 1.24.5
 
 require (
 	github.com/ethereum/go-ethereum v1.16.2
