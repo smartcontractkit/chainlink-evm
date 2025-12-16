05f27a8a750f18a2688b542781d1d918dc70803b
Alex Kuznicki <alexmkuz@gmail.com>
2025-12-16 09:04:44 -0700

    NoOpFeeManager discount public view support + add tests

--- contracts/src/v0.8/llo-feeds/v0.5.0/NoOpFeeManager.sol
+++ contracts/src/v0.8/llo-feeds/v0.5.0/NoOpFeeManager.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+import {ITypeAndVersion} from "../../shared/interfaces/ITypeAndVersion.sol";
+import {Common} from "../libraries/Common.sol";
 import {IFeeManager} from "./interfaces/IFeeManager.sol";
 import {IVerifierFeeManager} from "./interfaces/IVerifierFeeManager.sol";
-import {Common} from "../libraries/Common.sol";
 import {IERC165} from "@openzeppelin/contracts@4.8.3/interfaces/IERC165.sol";
 
 /**
@@ -12,12 +13,22 @@ import {IERC165} from "@openzeppelin/contracts@4.8.3/interfaces/IERC165.sol";
  * @dev All functions return successfully without performing any fee collection or state changes.
  *      Any ETH sent to payable functions is refunded to the subscriber.
  */
-contract NoOpFeeManager is IFeeManager {
+contract NoOpFeeManager is IFeeManager, ITypeAndVersion {
   /// @notice Error thrown when ETH refund fails
   error RefundFailed();
 
+  /// @notice The scalar representing 100% discount (1e18 = 100%)
+  uint64 private constant PERCENTAGE_SCALAR = 1e18;
+
+  /// @inheritdoc ITypeAndVersion
+  function typeAndVersion() external pure override returns (string memory) {
+    return "NoOpFeeManager 0.5.0";
+  }
+
   /// @inheritdoc IERC165
-  function supportsInterface(bytes4 interfaceId) external pure override returns (bool) {
+  function supportsInterface(
+    bytes4 interfaceId
+  ) external pure override returns (bool) {
     return interfaceId == this.processFee.selector || interfaceId == this.processFeeBulk.selector;
   }
 
@@ -44,12 +55,14 @@ contract NoOpFeeManager is IFeeManager {
     bytes memory,
     address
   ) external pure override returns (Common.Asset memory fee, Common.Asset memory reward, uint256 appliedDiscount) {
-    // Return zero fee, zero reward, zero discount
-    return (fee, reward, appliedDiscount);
+    // Return zero fee, zero reward, 100% discount (1e18) to indicate no fees are charged
+    return (fee, reward, PERCENTAGE_SCALAR);
   }
 
   /// @inheritdoc IFeeManager
-  function setNativeSurcharge(uint64) external override {
+  function setNativeSurcharge(
+    uint64
+  ) external override {
     // No-op
   }
 
@@ -69,7 +82,9 @@ contract NoOpFeeManager is IFeeManager {
   }
 
   /// @inheritdoc IFeeManager
-  function payLinkDeficit(bytes32) external override {
+  function payLinkDeficit(
+    bytes32
+  ) external override {
     // No-op
   }
 
@@ -79,16 +94,31 @@ contract NoOpFeeManager is IFeeManager {
   }
 
   /**
+   * @notice Returns 100% discount for any subscriber/feedId/token combination
+   * @dev Replicates public mapping getter signature from FeeManager for backwards compatibility
+   */
+  function s_subscriberDiscounts(address, bytes32, address) external pure returns (uint256) {
+    return PERCENTAGE_SCALAR;
+  }
+
+  /**
+   * @notice Returns 100% discount for any subscriber/token combination
+   * @dev Replicates public mapping getter signature from FeeManager for backwards compatibility
+   */
+  function s_globalDiscounts(address, address) external pure returns (uint256) {
+    return PERCENTAGE_SCALAR;
+  }
+
+  /**
    * @notice Refunds any ETH sent to the contract
    * @param recipient The address to refund ETH to
    */
-  function _refund(address recipient) internal {
+  function _refund(
+    address recipient
+  ) internal {
     if (msg.value > 0) {
-      (bool success, ) = payable(recipient).call{value: msg.value}("");
+      (bool success,) = payable(recipient).call{value: msg.value}("");
       if (!success) revert RefundFailed();
     }
   }
 }
-
-
-
