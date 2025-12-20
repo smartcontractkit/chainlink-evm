fae095ff15c57570702be2734ede5327b0d8aab4
Finley Decker <finley.decker@smartcontract.com>
2025-12-19 15:27:26 -1000

    add jovay to rpc tests

--- pkg/client/rpc_client_test.go
+++ pkg/client/rpc_client_test.go
@@ -395,6 +395,7 @@ func TestRPCClient_SubscribeFilterLogs(t *testing.T) {
 			{Name: "Hedera", ChainType: chaintype.ChainHedera},
 			{Name: "Rootstock", ChainType: chaintype.ChainRootstock},
 			{Name: "Pharos", ChainType: chaintype.ChainPharos},
+			{Name: "Jovay", ChainType: chaintype.ChainJovay},
 		}
 
 		testCases := []struct {
