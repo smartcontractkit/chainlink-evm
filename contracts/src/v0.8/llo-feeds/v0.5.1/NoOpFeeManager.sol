ede880bda63aed688d3c847732d1d4f47dede397
Alex Kuznicki <alexmkuz@gmail.com>
2026-01-21 17:25:22 -0500

    add public getters required for drop-in integration replacement of FeeManager

--- contracts/src/v0.8/llo-feeds/v0.5.1/NoOpFeeManager.sol
+++ contracts/src/v0.8/llo-feeds/v0.5.1/NoOpFeeManager.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.19;
 import {ITypeAndVersion} from "../../shared/interfaces/ITypeAndVersion.sol";
 import {Common} from "../libraries/Common.sol";
 import {IFeeManager} from "./interfaces/IFeeManager.sol";
+import {IRewardManager} from "./interfaces/IRewardManager.sol";
 import {IVerifierFeeManager} from "./interfaces/IVerifierFeeManager.sol";
 import {IERC165} from "@openzeppelin/contracts@4.8.3/interfaces/IERC165.sol";
 
@@ -12,6 +13,8 @@ import {IERC165} from "@openzeppelin/contracts@4.8.3/interfaces/IERC165.sol";
  * @notice A no-op implementation of IFeeManager that does not collect fees.
  * @dev All functions return successfully without performing any fee collection or state changes.
  *      Any ETH sent to payable functions is refunded to the subscriber.
+ *      Constructor parameters are stored for interface compatibility with integrators
+ *      who call i_linkAddress, i_nativeAddress, or i_rewardManager before getFeeAndReward().
  */
 contract NoOpFeeManager is IFeeManager, ITypeAndVersion {
   /// @notice Error thrown when ETH refund fails
@@ -19,6 +22,29 @@ contract NoOpFeeManager is IFeeManager, ITypeAndVersion {
 
   /// @notice The scalar representing 100% discount (1e18 = 100%)
   uint64 private constant PERCENTAGE_SCALAR = 1e18;
+
+  /// @notice The LINK token address (required for interface compatibility)
+  address public immutable i_linkAddress;
+
+  /// @notice The native token address (required for interface compatibility)
+  address public immutable i_nativeAddress;
+
+  /// @notice The reward manager address (required for interface compatibility)
+  IRewardManager public immutable i_rewardManager;
+
+  /**
+   * @notice Construct the NoOpFeeManager contract
+   * @param _linkAddress The address of the LINK token (for interface compatibility)
+   * @param _nativeAddress The address of the wrapped native token (for interface compatibility)
+   * @param _rewardManagerAddress The address of the reward manager (for interface compatibility)
+   * @dev These addresses are not used internally but are required for compatibility
+   *      with integrators who call these getters before getFeeAndReward().
+   */
+  constructor(address _linkAddress, address _nativeAddress, address _rewardManagerAddress) {
+    i_linkAddress = _linkAddress;
+    i_nativeAddress = _nativeAddress;
+    i_rewardManager = IRewardManager(_rewardManagerAddress);
+  }
 
   /// @inheritdoc ITypeAndVersion
   function typeAndVersion() external pure override returns (string memory) {
@@ -109,6 +135,15 @@ contract NoOpFeeManager is IFeeManager, ITypeAndVersion {
   // solhint-disable-next-line func-name-mixedcase
   function s_globalDiscounts(address, address) external pure returns (uint256) {
     return PERCENTAGE_SCALAR;
+  }
+
+  /**
+   * @notice Returns 0 surcharge since no fees are charged
+   * @dev Replicates public state variable getter from FeeManager for backwards compatibility
+   */
+  // solhint-disable-next-line func-name-mixedcase
+  function s_nativeSurcharge() external pure returns (uint256) {
+    return 0;
   }
 
   /**
