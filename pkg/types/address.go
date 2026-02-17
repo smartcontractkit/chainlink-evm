f8c9ff2b73c1a758f1e22d15231b8a0e09c3bad0
pavel-raykov <pavel.raykov@smartcontract.com>
2026-02-17 14:23:46 +0100

    Minor.

--- pkg/types/address.go
+++ pkg/types/address.go
@@ -1,9 +1,23 @@
 package types
 
 import (
+	"github.com/ethereum/go-ethereum/common"
+
 	"github.com/smartcontractkit/chainlink-common/keystore/corekeys/ethkey"
 )
 
 type EIP55Address = ethkey.EIP55Address
 
 type EIP55AddressCollection = ethkey.EIP55AddressCollection
+
+func NewEIP55Address(s string) (EIP55Address, error) {
+	return ethkey.NewEIP55Address(s)
+}
+
+func MustEIP55Address(s string) EIP55Address {
+	return ethkey.MustEIP55Address(s)
+}
+
+func EIP55AddressFromAddress(a common.Address) EIP55Address {
+	return ethkey.EIP55AddressFromAddress(a)
+}
