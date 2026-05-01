6301745f351dcf3708e52fc47e0eb33c62374b88
amit-momin <amit.momin@smartcontract.com>
2026-05-01 11:41:24 -0500

    Updated Mantle fatal error regex

--- pkg/client/errors_test.go
+++ pkg/client/errors_test.go
@@ -446,6 +446,8 @@ func Test_Eth_Errors_Fatal(t *testing.T) {
 		{"Failed to serialize transaction: oversized data. max: 1000000; actual: 1000000", true, "zkSync"},
 
 		{"failed to forward tx to sequencer, please try again. Error message: 'invalid sender'", true, "Mantle"},
+		{"failed to forward tx to sequencer, err: 'intrinsic gas too low: gas 5000000, minimum needed 93897576'", true, "Mantle"},
+		{"failed to forward tx to sequencer, err: 'exceeds block gas limit'", true, "Mantle"},
 
 		{"client error fatal", true, "tomlConfig"},
 		{"[Request ID: d9711488-4c1e-4af2-bc1f-7969913d7b60] Error invoking RPC: transaction 0.0.4425573@1718213476.914320044 failed precheck with status INVALID_SIGNATURE", true, "hedera"},
