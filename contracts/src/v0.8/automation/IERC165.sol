b93f75a17530497a7522225853d7d2cc2b1be2d8
bitrider23 <santino.fajardo@smartcontract.com>
2026-07-08 14:40:29 -0300

    Update IERC165

--- contracts/src/v0.8/automation/IERC165.sol
+++ contracts/src/v0.8/automation/IERC165.sol
@@ -1,12 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.2;
+// OpenZeppelin Contracts (last updated v5.4.0) (utils/introspection/IERC165.sol)
 
+pragma solidity >=0.4.16;
+
+/**
+ * @dev Interface of the ERC-165 standard, as defined in the
+ * https://eips.ethereum.org/EIPS/eip-165[ERC].
+ *
+ * Implementers can declare support of contract interfaces, which can then be
+ * queried by others ({ERC165Checker}).
+ *
+ * For an implementation, see {ERC165}.
+ */
 interface IERC165 {
-    /// @notice Query if a contract implements an interface
-    /// @param interfaceID The interface identifier, as specified in ERC-165
-    /// @dev Interface identification is specified in ERC-165. This function
-    /// uses less than 30,000 gas.
-    /// @return `true` if the contract implements `interfaceID` and
-    /// `interfaceID` is not 0xffffffff, `false` otherwise
-    function supportsInterface(bytes4 interfaceID) external view returns (bool);
+  /**
+   * @dev Returns true if this contract implements the interface defined by
+   * `interfaceId`. See the corresponding
+   * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[ERC section]
+   * to learn more about how these ids are created.
+   *
+   * This function call must use less than 30 000 gas.
+   */
+  function supportsInterface(
+    bytes4 interfaceId
+  ) external view returns (bool);
 }
