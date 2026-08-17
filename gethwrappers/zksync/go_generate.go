08aae5d5c6549ab0a75d5d9ab6fb2b59b29eafca
Rens Rooimans <github@rensrooimans.nl>
2026-08-17 09:54:33 +0200

    rm unused automation contracts

--- gethwrappers/zksync/go_generate.go
+++ gethwrappers/zksync/go_generate.go
@@ -7,5 +7,3 @@ package gethwrappers
 //go:generate go run ../generation/zksync/wrap.go shared Multicall3 multicall3 latest
 //go:generate go run ../generation/zksync/wrap.go shared WETH9ZKSync weth9_zksync latest
 //go:generate go run ../generation/zksync/wrap.go shared MockV3Aggregator mock_v3_aggregator_contract latest
-
-//go:generate go run ../generation/zksync/wrap.go automation MockETHUSDAggregator mock_ethusd_aggregator_wrapper latest
